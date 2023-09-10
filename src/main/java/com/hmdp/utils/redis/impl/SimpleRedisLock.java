76dc0c71785571ca81241b24335c03322441ee8e
CVToolMan <1292307821@qq.com>
2023-09-10 15:49:56 +0800

    4、分布式锁简单实现，lua脚本解决判断和删除非原子性问题

--- src/main/java/com/hmdp/utils/redis/impl/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/redis/impl/SimpleRedisLock.java
@@ -2,8 +2,11 @@ package com.hmdp.utils.redis.impl;
 
 import cn.hutool.core.lang.UUID;
 import com.hmdp.utils.redis.ILock;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,6 +18,15 @@ public class SimpleRedisLock implements ILock {
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
@@ -32,12 +44,14 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unLock() {
-        //获取线程标识
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        //获取锁中的ID
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+//        //获取线程标识
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        //获取锁中的ID
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if (threadId.equals(id)) {
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+        //lu脚本将判断和删除写成同一条语句
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX + name), ID_PREFIX + Thread.currentThread().getId());
     }
 }
